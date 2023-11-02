82af1bbf1f9e5366de832f1043cec4003314dfea
frrhhnn <farhankhr58@gmail.com>
2023-11-02 14:50:13 +0700

    feat: add petunjuk dan add input toleransi error

--- uts_iterasi.py
+++ uts_iterasi.py
@@ -4,11 +4,18 @@
 fungsi_fx = lambda persamaan, x : float(eval(persamaan))  #lambda ni sama aja kaya buat fungsi biasa, kek diatas
 fungsi_gx = lambda persamaan, x  : float(eval(persamaan))
 
-persamaan_fx = input('Masukkan persamaan f(x) : ')
-persamaan_gx = input('Masukkan persamaan g(x) : ')
-xiAwal = int(input('Masukkan X Awal : '))
-n = int(input("Masukkan nilai n : "))
-e = float(0.0001)
+print("\nPentunjuk cara input fungsi f(x) :")
+print("Ex: x**2 - 2*x - 3")
+
+print("\nPentunjuk cara input fungsi g(x) :")
+print("Ex: 3/(x - 2)")
+
+persamaan_fx = input("\nMasukkan persamaan f(x)\t\t: ")
+persamaan_gx = input("Masukkan persamaan g(x)\t\t: ")
+xiAwal = int(input("Masukkan X Awal\t\t\t: "))
+n = int(input("Masukkan nilai max iterasi\t: "))
+e = float(input("Masukkan nilai toleransi error\t:"))
+
 fxi = fungsi_fx(persamaan_fx, xiAwal)
 gxi = fungsi_gx(persamaan_gx, xiAwal)
 
