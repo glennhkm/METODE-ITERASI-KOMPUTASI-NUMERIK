cfb374d766f9124fe96f0677ae3b24f534ed4ef2
frrhhnn <farhankhr58@gmail.com>
2023-11-06 00:03:35 +0700

    Rapiin code

--- uts_iterasi.py
+++ uts_iterasi.py
@@ -9,14 +9,12 @@ def get_float_input(prompt):
         except ValueError:
             print("Input tidak valid! Mohon periksa kembali input Anda")
 
-# lambda ni sama aja kaya buat fungsi biasa, kek diatas
+
+def fungsi(persamaan, x): 
+    return float(eval(persamaan))
 
 
-def fungsi_fx(persamaan, x): return float(eval(persamaan))
-def fungsi_gx(persamaan, x): return float(eval(persamaan))
-
-
-print("=====Panduan penggunaan operator=====")
+print("\n==== Panduan penggunaan operator ====")
 print("| Operator tambah : +               |")
 print("| Operator kurang : -               |")
 print("| Operator kali : *                 |")
@@ -24,14 +22,14 @@ print("| Operator bagi : /                 |")
 print("| Operator pangkat : **             |")
 print("| Operator akar : math.sqrt(x)      |")
 print("| Operator EXP : math.exp(x)        |")
-print("=====================================")
+print("=====================================\n")
 
-persamaan_fx = input('Masukkan persamaan f(x) : ')
-persamaan_gx = input('Masukkan persamaan g(x) : ')
+persamaan_fx = input('Masukkan persamaan f(x)\t\t\t: ')
+persamaan_gx = input('Masukkan persamaan g(x)\t\t\t: ')
 
-xiAwal = get_float_input("Masukkan pendekatan awal (x0): ")
-n = get_float_input("Masukkan jumlah maksimum iterasi (N): ")
-e = get_float_input("Masukkan toleransi error (e): ")
+xiAwal = get_float_input("Masukkan pendekatan awal (x0)\t\t: ")
+n = get_float_input("Masukkan jumlah maksimum iterasi (N)\t: ")
+e = get_float_input("Masukkan toleransi error (e)\t\t: ")
 
 print()
 print("="*83)
@@ -39,10 +37,10 @@ print(f"{'|':<3}{'Iterasi':<10}{'|':<3}{'xi':<20}{'|':<3}{'g(xi)':<20}{'|':<3}{'
 print("="*83)
 
 for i in range(1, int(n) + 2):
-    gxi = fungsi_gx(persamaan_gx, xiAwal)
-    fxi = fungsi_fx(persamaan_fx, xiAwal)
+    gxi = fungsi(persamaan_gx, xiAwal)
+    fxi = fungsi(persamaan_fx, xiAwal)
 
-    print(f"{'|':<3}{i:<10}{'|':<3}{xiAwal:<20}{'|':<3}{gxi:<20}{'|':<3}{fxi:<20}{'|':<1}")
+    print(f"{'|':<3}{i:<10}{'|':<3}{xiAwal:<20.6f}{'|':<3}{gxi:<20.6f}{'|':<3}{fxi:<20.6f}{'|':<1}")
 
     if fxi < e:
         print("-"*83)  # End of the table
@@ -51,7 +49,7 @@ for i in range(1, int(n) + 2):
         break
     xiAwal = gxi
 
-    if i == n + 2:
-        print("-"*83)  # End of the table
-        print(
-            f"\nMaksimum jumlah iterasi telah tercapai, Maka nilai x akan diambil pada iterasi + 1, yaitu x = {xiAwal}.\n")
+else:
+    print("-"*83)  # End of the table
+    print(
+        f"\nMaksimum jumlah iterasi telah tercapai, Maka nilai x akan diambil pada (Nilai maksimum iterasi + 1), yaitu x = {xiAwal:.2f}.\n")
